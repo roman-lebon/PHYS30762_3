6895dcd1db8d64e5fd5d95e8b3c1966743c47c40
Roman le Bon <romanlebon@Romans-MacBook-Pro.local>
2026-03-10 12:58:25 +0000

    Added initial Source class structure

---
+++ source.h
@@ -0,0 +1,26 @@
+#ifndef SOURCE_H
+#define SOURCE_H
+
+#include <string>
+
+// Source class - represents radioactive source 
+
+class Source {
+    private: // Private variables store the data describing the source (cannot be acessed from outside the class)
+
+        std::string type; // Which element the source is (e.g Na-22)
+        std::string date; // Source acquisition date
+        double activity; // Source activity (in Bq/kg)
+        int id; // Source ID 
+
+    public:
+        // Default constructors 
+        Source();
+        ~Source(); 
+
+        // Member functions allowing us to interact with the object
+
+        void printSource();
+};       
+
+#endif 
